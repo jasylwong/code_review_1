e4f45301e84af0cccb90ce7ccbf0c2eb46e50ee2
Jason <jasylwong@gmail.com>
2020-06-09 11:55:43 +0100

    copy frontend build into back, and use express.static

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -7,6 +7,7 @@ const cors = require('cors')
 app.use(bodyParser.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-body'));
 app.use(cors())
+app.use(express.static('build'))
 
 morgan.token('post-body', function(req, res) {
   // if (req.body !== {}) {
