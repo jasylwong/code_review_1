5066ecd14e6ebbb404631ca57df454d3c03be9cc
Jason <jasylwong@gmail.com>
2020-06-12 12:58:42 +0100

    uibuild

---
+++ part3/phonebook/build/precache-manifest.bcf10ef0c1b0c64e1c02a7f647919c85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d071aae1c52d505143434cffb0c2700",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a61eaf28cba852f9743d",
+    "url": "/static/css/main.55c21a8c.chunk.css"
+  },
+  {
+    "revision": "3f41d429740e9d2e0093",
+    "url": "/static/js/2.3d7c9fe8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3d7c9fe8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a61eaf28cba852f9743d",
+    "url": "/static/js/main.6d123519.chunk.js"
+  },
+  {
+    "revision": "68d5127cb03992670844",
+    "url": "/static/js/runtime-main.684b035f.js"
+  }
+]);
