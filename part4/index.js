71408b4633019e29109461f134ef6af65dff1b35
Jason <jasylwong@gmail.com>
2020-06-15 18:19:04 +0100

    extract routing to separate controller module

--- part4/index.js
+++ part4/index.js
@@ -1,12 +1,8 @@
 const http = require('http')
-const express = require('express')
-// const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const Blog = require('./models/blog')
 const config = require('./utils/config')
 const app = require('./app')
+const logger = require('./utils/logger')
 
 app.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`)
+  logger.info(`Server running on port ${config.PORT}`)
 })
