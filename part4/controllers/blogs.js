235ee5a378f333ab83fdc8119df85034a059137f
Jason <jasylwong@gmail.com>
2020-06-25 20:03:57 +0100

    4.19 bloglist expansion step7 - can only add blogs if valid token sent with POST request

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,4 +1,5 @@
 const blogRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
@@ -7,11 +8,24 @@ blogRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
 blogRouter.post('/', async (request, response) => {
-  if (request.body.title && request.body.url) {
-    
-    const users = await User.find({})
-    const user = users[0]
+  const body = request.body
+
+  if (body.title && body.url) {
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
 
     const blog = new Blog({
       ...request.body,
