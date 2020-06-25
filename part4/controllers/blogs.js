0cd4abe96c33bfe14adb6afcdee757f4221c4075
Jason <jasylwong@gmail.com>
2020-06-25 19:39:11 +0100

    4.17 bloglist expansion step5 - blogs and users linked

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,5 +1,6 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
@@ -7,9 +8,20 @@ blogRouter.get('/', async (request, response) => {
 })
 
 blogRouter.post('/', async (request, response) => {
-  if (request.body.title && request.body.url) { 
-    const blog = new Blog(request.body)
+  if (request.body.title && request.body.url) {
+    
+    const users = await User.find({})
+    const user = users[0]
+
+    const blog = new Blog({
+      ...request.body,
+      user: user._id
+    })
+
     const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog.id)
+    user.save()
+
     response.status(201).json(savedBlog.toJSON())
   } else {
     response.status(400).end()
