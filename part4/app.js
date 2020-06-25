6397645fe1458d657890c61cb5f8c55c6f9efe5e
Jason <jasylwong@gmail.com>
2020-06-25 19:57:25 +0100

    4.18: bloglist expansion step6 - implement token-based authentication

--- part4/app.js
+++ part4/app.js
@@ -5,6 +5,7 @@ const app = express()
 const cors = require('cors')
 const blogRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
@@ -23,5 +24,6 @@ app.use(express.json())
 
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
