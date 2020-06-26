26ffb01c16c70bfdf452e3356dc8012ebbf7eaf5
Jason <jasylwong@gmail.com>
2020-06-26 11:34:57 +0100

    rebase users.js controller

--- part4/controllers/users.js
+++ part4/controllers/users.js
@@ -3,20 +3,24 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({}).populate('blogs')
+  const users = await User.find({}).populate('blogs', { title: 1, url: 1, likes: 1, author: 1 })
   response.json(users.map(user => user.toJSON()))
 })
 
 usersRouter.post('/', async (request, response) => {
-  const body = request.body
+  const { password, username, name} = request.body
+
+  if ( !password || password.length < 3) {
+    return response.status(400).send({
+      error: 'password must min length 3'
+    })
+  }
 
   const saltRounds = 10
-  const passwordHash = await bcrypt.hash(body.password, saltRounds)
+  const passwordHash = await bcrypt.hash(password, saltRounds)
 
   const user = new User({
-    username: body.username,
-    name: body.name,
-    passwordHash
+    username, name, passwordHash
   })
 
   const savedUser = await user.save()
